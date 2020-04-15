3d033186d685e39850b9f5a8ac4d572ab7d19865
German <futuregerm@gmail.com>
2020-04-15 13:35:41 -0600

    Updates

---
+++ precache-manifest.61e15bc921839a2c9b17f5da274e547b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c462f33b2bc52410a20049dbec60c80e",
+    "url": "/login-auth/index.html"
+  },
+  {
+    "revision": "a990b31c2fecb63e2b85",
+    "url": "/login-auth/static/css/main.e8503fac.chunk.css"
+  },
+  {
+    "revision": "c14ba2557d2d2a010a5d",
+    "url": "/login-auth/static/js/2.98dd21c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login-auth/static/js/2.98dd21c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a990b31c2fecb63e2b85",
+    "url": "/login-auth/static/js/main.1e806a6d.chunk.js"
+  },
+  {
+    "revision": "4d558995f292c8b38111",
+    "url": "/login-auth/static/js/runtime-main.f206c571.js"
+  },
+  {
+    "revision": "bb33c1bf28399a7f157840049f63d81b",
+    "url": "/login-auth/static/media/logo.bb33c1bf.svg"
+  }
+]);
